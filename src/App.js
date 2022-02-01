209344cc34e2cf17b35305f889d7416e3ced1176
Your Name <you@example.com>
2022-02-01 20:15:05 +0900

    2022-02-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+/*eslint-disable*/
 import './App.css';
+import React, { useState } from 'react';
+import { Navbar, Container, Nav, NavDropdown, } from 'react-bootstrap';
+import Data from "./data.js"
 
 function App() {
+
+  let [shoes, setShoes] = useState(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoesShope</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className='jumbo'>
+        <div className='second-jumbo'>
+          <h1>20% Season Off</h1>
+          <p>this is simple page</p>
+          <button>Learn more</button>
+        </div>
+      </div>
+      <div className='container'>
+        <div className="row">
+          {shoes.map((a, i) => {
+            return <StuffShoes i={i}
+              shoesName={shoes[i].title} shoesContent={shoes[i].content} shoesPrice={shoes[i].price}
+            ></StuffShoes>
+          })
+          }
+        </div>
+      </div>
     </div>
   );
 }
+
+function StuffShoes(props) {
+  return (
+    <div className='col-md-4'>
+      <img alt='shoes1' src={"https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"} width="100%"></img>
+      <h4>{props.shoesName}</h4>
+      <p>{props.shoesContent} & {props.shoesPrice}₩ </p>
+    </div >
+  );
+};
 
 export default App;
